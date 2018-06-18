c64804698cb5e4e87a9f476ceb1c34fc040c3435
Alexander Molnar <mathic@gmail.com>
2018-06-18 16:43:47 -0400

    Got new screenshots of apps, all of same dimensions

--- src/projects/cssProjects.js
+++ src/projects/cssProjects.js
@@ -2,42 +2,42 @@ const cssProjects = [
   'Projects from the 50 Days of CSS challenge', [
     {
       title: 'Beaver',
-      image: '/Images/css-beaver.jpg',
+      image: '/Images/css-beaver.png',
       alt: 'Screenshot of CSS beaver',
       link: 'https://codepen.io/brokentriathlete/full/rmYoXr/',
       tools: 'Codepen'
     },
     {
       title: 'Notepad',
-      image: '/Images/css-notepad.jpg',
+      image: '/Images/css-notepad.png',
       alt: 'Screenshot of CSS notepad',
       link: 'https://codepen.io/brokentriathlete/full/mmLRom/',
       tools: 'Codepen'
     },
     {
       title: 'Burger',
-      image: '/Images/css-burger.jpg',
+      image: '/Images/css-burger.png',
       alt: 'Screenshot of CSS burger',
       link: 'https://codepen.io/brokentriathlete/full/bWmXKg/',
       tools: 'Codepen'
     },
     {
       title: 'Wolverine',
-      image: '/Images/css-wolverine.jpg',
+      image: '/Images/css-wolverine.png',
       alt: 'Screenshot of CSS wolverine',
       link: 'https://codepen.io/brokentriathlete/full/mwEVLM/',
       tools: 'Codepen'
     },
     {
       title: 'Bar Chart',
-      image: '/Images/css-bar-chart.jpg',
+      image: '/Images/css-bar-chart.png',
       alt: 'Screenshot of CSS bar chart',
       link: 'https://codepen.io/brokentriathlete/full/KqQyyV/',
       tools: 'Codepen'
     },
     {
       title: 'Werewolf',
-      image: '/Images/css-werewolf.jpg',
+      image: '/Images/css-werewolf.png',
       alt: 'Screenshot of CSS werewolf',
       link: 'https://codepen.io/brokentriathlete/full/BZazxe/',
       tools: 'Codepen'
--- src/projects/reactProjects.js
+++ src/projects/reactProjects.js
@@ -1,55 +1,55 @@
 const reactProjects = [ 'React', [
   {
     title: 'Roguelike Dungeon Crawler',
-    image: '/Images/dungeon-crawler.jpg',
+    image: '/Images/dungeon-crawler.png',
     alt: 'Screenshot of Dungeon Crawler app',
     link: 'https://BTmathic.github.io/dungeon-crawler',
     tools: 'Webpack'
   }, {
     title: 'Expense Tracker',
-    image: '/Images/expensify.jpg',
+    image: '/Images/expensify.png',
     alt: 'Screenshot of Dungeon Crawler app',
     link: 'https://react-expensify-mathic.herokuapp.com/dashboard',
     tools: 'Redux, React-Router, Webpack'
   }, {
     title: "Conway's Game of Life",
-    image: '/Images/game-of-life.jpg',
+    image: '/Images/game-of-life.png',
     alt: 'Screenshot of Game of Life app',
     link: 'https://BTmathic.github.io/game-of-life',
     tools: 'Webpack'
   }, {
     title: 'Recipe Box',
-    image: '/Images/recipe-box.jpg',
+    image: '/Images/recipe-box.png',
     alt: 'Screenshot of Recipe Box app',
     link: 'https://BTmathic.github.io/recipe-box',
     tools: 'React Modal, Webpack'
   }, {
     title: 'Tic Tac Toe',
-    image: '/Images/tic-tac-toe.jpg',
+    image: '/Images/tic-tac-toe.png',
     alt: 'Screenshot of Tic Tac Toe app',
     link: 'https://BTmathic.github.io/tic-tac-toe',
     tools: 'SVGO, Webpack'
   }, {
     title: 'Pomodoro Clock',
-    image: '/Images/pomodoro-clock.jpg',
+    image: '/Images/pomodoro-clock.png',
     alt: 'Screenshot of Pomodoro Clock app',
     link: 'https://BTmathic.github.io/pomodoro-clock',
     tools: 'Webpack'
-  }, {
+  }, /* { // skip since API is down again
     title: 'FCC Camper Leaderboard',
-    image: '/Images/camper-leaderboard.jpg',
+    image: '/Images/camper-leaderboard.png',
     alt: 'Screenshot of FCC Camper Leaderboard app',
     link: 'https://BTmathic.github.io/camper-leaderboard',
     tools: 'Webpack'
-  }, {
+  },*/ {
     title: 'Calculator',
-    image: '/Images/calculator.jpg',
+    image: '/Images/calculator.png',
     alt: 'Screenshot of Calculator app',
     link: 'https://BTmathic.github.io/calculator',
     tools: 'Webpack'
   }, {
     title: 'GitHub Markdown Previewer',
-    image: '/Images/markdown-previewer.jpg',
+    image: '/Images/markdown-previewer.png',
     alt: 'Screenshot of Github Markup Interpreter app',
     link: 'https://BTmathic.github.io/markdown-previewer',
     tools: 'Webpack'
