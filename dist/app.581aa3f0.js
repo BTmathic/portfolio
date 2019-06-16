356864e1f0f1b1d7591b90841fd05ae6dafac768
Alexander Molnar <mathic@gmail.com>
2019-06-16 17:30:51 -0400

    Remove unnecessary sass script from package.json

--- dist/app.581aa3f0.js
+++ dist/app.581aa3f0.js
@@ -83528,7 +83528,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43893" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35929" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
